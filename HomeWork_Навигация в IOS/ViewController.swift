ee95ad5ee55763be97b3f1063c008965f7e102e6
Artyom Sizov <sizov_artem@bk.ru>
2021-05-22 12:24:36 +0300

    HomeWork_1.3

---
+++ HomeWork_Навигация в IOS/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  HomeWork_Навигация в IOS
+//
+//  Created by Артем Сизов on 20.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    struct Post {
+        var title:String
+    }
+}
+
